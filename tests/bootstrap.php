45acf213efcfab5dd7750962606a569f917e3cf1
paladox <paladox@users.noreply.github.com>
2024-12-27 15:15:39 +0000

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ if ( PHP_SAPI !== 'cli' && PHP_SAPI !== 'phpdbg' ) {
 	die( 'Not an entry point' );
 }
 
-error_reporting( -1 );
+error_reporting( E_ALL | E_STRICT );
 date_default_timezone_set( 'UTC' );
 ini_set( 'display_errors', '1' );
 
